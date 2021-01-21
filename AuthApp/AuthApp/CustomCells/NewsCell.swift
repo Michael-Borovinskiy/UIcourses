a525f08b2b04198d863be090ed1b01d80d2b5a84
Michael.Bor <m.borovinskiy@mail.ru>
2021-01-21 23:13:49 +0300

    update model news to class Comment and other new entities, plus functionality to count of looks for news, and action and animation to button likes for news screen

--- AuthApp/AuthApp/CustomCells/NewsCell.swift
+++ AuthApp/AuthApp/CustomCells/NewsCell.swift
@@ -7,8 +7,18 @@
 
 import UIKit
 
-class NewsCell: UITableViewCell { 
 
+class NewsCell: UITableViewCell {
+
+ 
+
+    var newsData: News?
+    var counterLikes: Int?
+    var isLiked: Bool = false
+    var id: Int?
+
+    
+    
     @IBOutlet weak var uiHeader: UIView!
     @IBOutlet weak var uiImageHeader: UIImageView!{
         didSet{
@@ -25,22 +35,128 @@ class NewsCell: UITableViewCell {
     @IBOutlet weak var forwardMessage: UIButton!
     @IBOutlet weak var buttonLooks: UIButton!
     
-    func setData (_ textLabel: String ,_ nameImg: String, _ nameImgHeader: String, _ uiUserName: String) {
+    func setData (text textLabel: String ,img nameImg: String, imgHeader nameImgHeader: String, userName uiUserName: String, countPhotoLikes photoLikes: Int, countNewsLikes newsLikes: Int, countComments comments: [Comment], countForwardMessage forwardMessage: Int, countLooksForNews looksForNews: Int) {
         self.uiLabel.text = textLabel
         self.uiImageView.image = UIImage(named: nameImg)
         self.uiImageHeader.image = UIImage(named: nameImgHeader)
         self.uiUserName.text = uiUserName
+        self.buttonLike.setTitle("\(newsLikes)", for: .normal)
+        self.commentLike.setTitle("\(comments.count)", for: .normal)
+        self.forwardMessage.setTitle("\(forwardMessage)", for: .normal)
+        
+        let newLooksCount = looksForNews + 1
+        newsData?.news[id!].looksForNews = newLooksCount             // обновление количества просмотров при загрузке ячейки
+        self.buttonLooks.setTitle("\(newLooksCount)", for: .normal)
+        
+        counterLikes = newsLikes
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         uiImageView.layer.cornerRadius = 3
+        buttonLike.addTarget(self, action: #selector(setLike), for: .touchUpInside)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
     }
+    
+    // MARK: Actions
+    
+    func setLikes (countOfLikes cnt: Int) {
+        self.buttonLike.setTitle("\(cnt)", for: .normal)
+        counterLikes = cnt
+    }
+    
+    func setIsLiked (bool isLiked: Bool) {
+        self.isLiked = isLiked
+        if isLiked {
+            buttonLike.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        }
+    }
+    
+        @objc func setLike() {
+            if !isLiked  {
+                animateLike()
+                counterLikes = counterLikes! + 1
+                self.buttonLike.setTitle(String(counterLikes!), for: .normal)
+                self.buttonLike.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+                newsData?.news[id!].newsLikes = counterLikes!
+                newsData?.news[id!].isLiked.toggle()
+                
+                isLiked.toggle()
+            }
+            else  {
+                animateLikeBack()
+                counterLikes = counterLikes! - 1
+                self.buttonLike.setTitle(String(counterLikes!), for: .normal)
+                self.buttonLike.setImage(UIImage(systemName: "heart"), for: .normal)
+                newsData?.news[id!].newsLikes = counterLikes!
+                newsData?.news[id!].isLiked.toggle()
+
+                
+                isLiked.toggle()
+            }
+        }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        uiLabel?.text = nil
+        uiImageView.image = nil
+        uiImageHeader.image = nil
+        uiUserName.text = nil
+        buttonLike.setTitle("", for: .normal)
+        commentLike.setTitle("", for: .normal)
+        forwardMessage.setTitle("", for: .normal)
+        buttonLooks.setTitle("", for: .normal)
+        buttonLike.setImage(UIImage(systemName: "heart"), for: .normal)  // для переиспользованиых ячеек обновление лайков
+        id = nil
+        counterLikes = nil
+        isLiked = false
+    }
+    
+    func animateLike () {
+        let animationsGroup = CAAnimationGroup()
+        animationsGroup.duration = 0.3
+        animationsGroup.fillMode = CAMediaTimingFillMode.backwards
+        
+        let translation = CABasicAnimation(keyPath: "position.y")
+        translation.toValue = 0.1
+        let alpha = CABasicAnimation(keyPath: "opacity")
+        alpha.toValue = 0
+
+        animationsGroup.animations = [translation, alpha]
+        
+        buttonLike.layer.add(animationsGroup,forKey: nil)
+        
+    }
+    
+    func animateLikeBack () {
+        let animationsGroup = CAAnimationGroup()
+        animationsGroup.duration = 0.3
+        animationsGroup.fillMode = CAMediaTimingFillMode.backwards
+        
+        let rotation : CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = 0.3
+        rotation.isCumulative = true
+        rotation.repeatCount = 66.0
+        rotation.speed = 60
+        
+        let scale: CABasicAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scale.fromValue = 1
+        scale.toValue = 0.3
+        
+        
+        let opacity : CABasicAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+        opacity.toValue = 0
+        animationsGroup.animations = [scale, opacity, rotation]
+        
+        buttonLike.layer.add(animationsGroup, forKey:  nil)
+        
+    }
+    
     
     
 }
--- AuthApp/AuthApp/News/NewsViewController.swift
+++ AuthApp/AuthApp/News/NewsViewController.swift
@@ -24,17 +24,36 @@ class NewsViewController: UIViewController, UITableViewDataSource ,UITableViewDe
     
     // MARK: DataSource
     
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return newsData.news.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "news", for: indexPath) as! NewsCell
-        cell.setData(newsData.news[indexPath.row].text,               newsData.news[indexPath.row].photo,
-                     newsData.news[indexPath.row].user.image,
-                     "\(newsData.news[indexPath.row].user.surname) \(newsData.news[indexPath.row].user.name)"
+        
+        cell.id = newsData.news[indexPath.row].id
+        
+        cell.setData(text: newsData.news[indexPath.row].text,
+                     img: newsData.news[indexPath.row].photo,
+                     imgHeader: newsData.news[indexPath.row].user.image,
+                     userName: "\(newsData.news[indexPath.row].user.surname) \(newsData.news[indexPath.row].user.name)",
+                     countPhotoLikes: newsData.news[indexPath.row].photoLikes,
+                     countNewsLikes: newsData.news[indexPath.row].newsLikes,
+                     countComments: newsData.news[indexPath.row].comments,
+                     countForwardMessage: newsData.news[indexPath.row].forwardMessages,
+                     countLooksForNews: newsData.news[indexPath.row].looksForNews
         )
+        cell.newsData = newsData
+        cell.setIsLiked(bool: newsData.news[indexPath.row].isLiked)
+        
+
         return cell
     }
     
