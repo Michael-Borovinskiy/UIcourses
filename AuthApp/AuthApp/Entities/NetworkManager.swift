b8fc17380cd115206b57a2c537a293fb906f379e
Michael.Bor <m.borovinskiy@mail.ru>
2021-02-02 00:08:07 +0300

    added VC for url connection with VK API and Network Manager for printing data to console

---
+++ AuthApp/AuthApp/Entities/NetworkManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkManager.swift
+//  AuthApp
+//
+//  Created by Михаил on 01.02.2021.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func getStartRequest () -> URLRequest {
+        
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "7746892"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.68")
+            ]
+        return URLRequest(url: urlComponents.url!)
+    }
+    
+    
+    func getRes (stringURL string: String) {
+        
+        let session = URLSession.shared
+
+        let task = session.dataTask(with: URL(string: string)!) { (data, response, error) in
+        let json = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
+            print(json!)
+        }
+
+        task.resume()
+    }
+    
+    
+//    func getResWithSearch (stringURL string: String) {
+//
+//        let session = URLSession.shared
+//
+//        let task = session.dataTask(with: URL(string: string)!) { (data, response, error) in
+//            print(data!)
+//        }
+//        task.resume()
+//    }
+
+}
